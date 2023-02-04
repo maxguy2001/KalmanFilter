a4dbcd133844831fddb42e341ebb11c966e0c564
maxguy2001 <max.guy2001@gmmail.com>
2023-02-04 17:41:58 +0000

    it all broke

--- kalman_filter.py
+++ kalman_filter.py
@@ -12,7 +12,7 @@ class KalmanFilter:
         self.measurement_covariance_matrix = np.eye(3)
 
         self.process_noise_covariance_matrix = np.eye(
-            3) * np.log(initial_timestamp)
+            3) * 0.0001
         self.error_covariance_matrix = np.zeros([3, 3])
         self.system_noise_covariance_matrix = np.eye(3)*0.001
 
@@ -29,25 +29,27 @@ class KalmanFilter:
         old state matrix
         """
         # TODO: consider more complex system models later
-        return np.array([[1, 0, 0], [self.time_delta, 1, 0], [self.time_delta**2, self.time_delta, 1]])
+        return np.array([[1, 0, 0], [self.time_delta, 1, 0], [self.time_delta**2, self.time_delta, 1]], dtype=object)
 
     # measurement update functions
 
     def getErrorCovarianceMatrix(self):
         # propagate from start point
-        self.error_covariance_matrix = ((self.transition_matrix @ self.measurement_covariance_matrix)
-                                        @ self.transition_matrix.T) + self.system_noise_covariance_matrix
+        state_transition_matrix = self.getStateTransitionMatrix()
+        self.error_covariance_matrix = ((state_transition_matrix @ self.measurement_covariance_matrix)
+                                        @ state_transition_matrix.T) + self.system_noise_covariance_matrix
         return None
 
     def getKalmanGain(self):
-        mat1 = self.error_covariance_matrix @ self.measurement_covriance_matrix.T
-        mat2 = self.measurement_covriance_matrix @ self.error_covariance_matrix @ self.measurement_covriance_matrix
-        kalman_gain = mat1 @ np.linalg.inv(mat2)
+        mat1 = self.error_covariance_matrix @ self.error_covariance_matrix.T
+        mat2 = self.error_covariance_matrix @ self.error_covariance_matrix @ self.error_covariance_matrix
+        print(mat1, mat2)
+        kalman_gain = np.matmul(mat1, np.linalg.pinv(mat2))
         return kalman_gain
 
-    def getStateEstimate(self, observed_state):
+    def getStateEstimate(self, observed_state, kalman_gain):
         new_estimate = self.new_state_estimate + \
-            (self.kalman_gain @ (observed_state - self.new_state_estimate))
+            (kalman_gain @ (observed_state - self.new_state_estimate))
         self.state_vector = new_estimate
         return None
 
@@ -74,7 +76,7 @@ class KalmanFilter:
 
         self.getErrorCovarianceMatrix()
         kalman_gain = self.getKalmanGain()
-        self.getStateEstimate(observed_state)
+        self.getStateEstimate(observed_state, kalman_gain)
         self.getStateCovarianceMatrix(kalman_gain)
 
     def estimate(self, current_timestamp, observed_state):
